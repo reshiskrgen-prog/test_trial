b97d4644231ecb32215243700a4be8e2816042bb
reshiskrgen-prog <reshi.skrgen@gmail.com>
2026-02-17 19:42:58 -0600

    Add files via upload

---
+++ code.py
@@ -0,0 +1,3 @@
+print("SHOP")
+print("Welcome! This is the shop! You have 50 dollars to spend, but you need to buy soap, water, and food, but you also have to buy a birthday gift for your friend! You also have to buy something for yourself. The objective is to manage your budget, and remember to try to pick good things, so you can make your happiness higher at the end!")
+food = input("What food will you buy, moss, pizza, or salad?")
